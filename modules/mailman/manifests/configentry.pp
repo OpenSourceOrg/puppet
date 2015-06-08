102edc75dc639ac22658b924e2dd6ab793788d97
Roland Mas <roland.mas@gnurandal.com>
2015-06-08 15:10:49 +0200

    Cleanup

--- modules/mailman/manifests/configentry.pp
+++ modules/mailman/manifests/configentry.pp
@@ -1,5 +1,6 @@
 define mailman::configentry ($value) {
-  $regex = "^$name[ =]"
+  $regex = "^$name[ =].*"
+  $file = "/etc/mailman/mm_cfg.py"
 
   exec { "uniquify_mmcfg_$name":
     require => [
@@ -7,16 +8,16 @@ define mailman::configentry ($value) {
                 Package['mailman'],
                 ],
     notify => Service['mailman'],
-    command => "awk '{ if (/$regex/) {found++;if (found == 1) print} else {print}} ; END { if (found == 0) print \"$name = \" }' /etc/mailman/mm_cfg.py | sponge /etc/mailman/mm_cfg.py",
+    command => "awk '{ if (/$regex/) {found++;if (found == 1) print} else {print}} ; END { if (found == 0) print \"$name = \" }' $file | sponge $file",
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-    unless => "test $(grep -c '$regex' /etc/mailman/mm_cfg.py) = 1"
+    unless => "test $(grep -c '$regex' $file) = 1"
   }
 
   exec { "set_mmcfg_$name":
-    command => "sed -i -e 's/$regex.*/$name = $value/' /etc/mailman/mm_cfg.py",
+    command => "sed -i -e 's/$regex/$name = $value/' $file",
     path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
     require => Exec["uniquify_mmcfg_$name"],
     notify => Service['mailman'],
-    unless => "grep -q '^$name = $value\$' /etc/mailman/mm_cfg.py"
+    unless => "grep -F -q -x '$name = $value' $file"
   }
 }
