d8a7b022e9661c687897521e7b60516fe198f62e
Roland Mas <roland.mas@gnurandal.com>
2015-06-08 11:43:21 +0200

    Show diffs during Puppet run

--- apply.sh
+++ apply.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 p=$(dirname $0)
-puppet apply --verbose --modulepath=$p/modules/ $p/manifests/site.pp "$@"
+puppet apply --verbose --show_diff --modulepath=$p/modules/ $p/manifests/site.pp "$@"
