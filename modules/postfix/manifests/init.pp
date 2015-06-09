3c0152149028432075cb72c4588e717b6f11850f
Roland Mas <roland.mas@gnurandal.com>
2015-06-09 14:59:02 +0200

    Install Amavis/ClamAV, and integrate them with Postfix

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -36,6 +36,7 @@ class postfix ($use_mailman = false, $destinations = []) {
                 "set home_mailbox Maildir/",
                 "set maximal_queue_lifetime 1d",
                 "set smtpd_recipient_restrictions 'reject_unknown_sender_domain, reject_unknown_recipient_domain, reject_unauth_pipelining, permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination'",
+                "set content_filter 'smtp-amavis:[127.0.0.1]:10024'",
                 ],
     lens => 'Postfix_Main.lns',
     incl => '/etc/postfix/main.cf',
@@ -75,5 +76,46 @@ class postfix ($use_mailman = false, $destinations = []) {
     chroot => 'n',
     command => 'pipe user=debian-spamd argv=/usr/bin/spamc -f -e /usr/sbin/sendmail -oi -f ${sender} ${recipient}',
     require => Service['spamassassin']
+  }
+
+  package { 'amavisd-new':
+    ensure => present
+  }
+  package { 'clamav-daemon':
+    ensure => present
+  }
+  package { 'clamav-freshclam':
+    ensure => present
+  }
+  postfix::postconf { 'smtp-amavis':
+    type => 'unix',
+    maxproc => 2,
+    command => 'smtp
+    -o smtp_data_done_timeout=1200
+    -o smtp_send_xforward_command=yes
+    -o disable_dns_lookups=yes
+    -o max_use=20',
+  }
+  postfix::postconf { '127.0.0.1:10025':
+    private => 'n',
+    command => 'smtpd
+    -o content_filter=
+    -o local_recipient_maps=
+    -o relay_recipient_maps=
+    -o smtpd_restriction_classes=
+    -o smtpd_delay_reject=no
+    -o smtpd_client_restrictions=permit_mynetworks,reject
+    -o smtpd_helo_restrictions=
+    -o smtpd_sender_restrictions=
+    -o smtpd_recipient_restrictions=permit_mynetworks,reject
+    -o smtpd_data_restrictions=reject_unauth_pipelining
+    -o smtpd_end_of_data_restrictions=
+    -o mynetworks=127.0.0.0/8
+    -o smtpd_error_sleep_time=0
+    -o smtpd_soft_error_limit=1001
+    -o smtpd_hard_error_limit=1000
+    -o smtpd_client_connection_count_limit=0
+    -o smtpd_client_connection_rate_limit=0
+    -o receive_override_options=no_header_body_checks,no_unknown_recipient_checks'
   }
 }
