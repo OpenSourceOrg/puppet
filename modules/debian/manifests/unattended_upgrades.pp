e610ebbe10b73f1c7a80bcc5dd83bbcfb43d6dc9
Stefano Zacchiroli <zack@upsilon.cc>
2015-06-17 18:51:28 +0200

    debian module: use 99local-* prefix for apt conf files

--- modules/debian/manifests/unattended_upgrades.pp
+++ modules/debian/manifests/unattended_upgrades.pp
@@ -1,7 +1,7 @@
 class debian::unattended_upgrades {
   package { 'unattended-upgrades': ensure => present }
 
-  file { '/etc/apt/apt.conf.d/99puppet-unattended-upgrades':
+  file { '/etc/apt/apt.conf.d/99local-unattended-upgrades':
     ensure  => present,
     content => template('debian/unattended-upgrades-apt-conf.erb'),
   }
