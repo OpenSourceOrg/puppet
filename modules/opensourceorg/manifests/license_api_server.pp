b7e26ad2934b96f0f2d04df69676bd8e7ec08f05
Stefano Zacchiroli <zack@upsilon.cc>
2016-04-22 07:21:02 -0400

    license_api_server: require apache and proxy modules

--- modules/opensourceorg/manifests/license_api_server.pp
+++ modules/opensourceorg/manifests/license_api_server.pp
@@ -3,12 +3,16 @@ class opensourceorg::license_api_server
 {
 
   package { 'opensource.org-api':
-    ensure => installed
+    ensure  => installed,
+    require => Package['apache2'],
   }
 
   file { $conffile:
     ensure  => present,
     notify  => Service['apache2']
   }
+
+  apache2::module { 'proxy': }
+  apache2::module { 'proxy_http': }
 
 }
