f027f9dee09b996adcfd37fcfef8255caa2b3601
Stefano Zacchiroli <zack@upsilon.cc>
2016-04-22 09:59:26 -0400

    license_api_server: ensure /srv/api.opensource.org dir exists

--- modules/opensourceorg/manifests/license_api_server.pp
+++ modules/opensourceorg/manifests/license_api_server.pp
@@ -1,6 +1,8 @@
-class opensourceorg::license_api_server
-  ($conffile = '/etc/api.opensource.org/licenses.json')
-{
+class opensourceorg::license_api_server (
+  $conffile = '/etc/api.opensource.org/licenses.json',
+  $data_dir = '/srv/api.opensource.org',
+  $user = 'www-data'
+) {
 
   package { 'opensource.org-api':
     ensure  => installed,
@@ -10,6 +12,13 @@ class opensourceorg::license_api_server
   file { $conffile:
     ensure  => present,
     notify  => Service['apache2']
+  }
+
+  file { $data_dir:
+    ensure => directory,
+    mode   => '0755',
+    owner  => $user,
+    group  => $user,
   }
 
   apache2::module { 'proxy': }
